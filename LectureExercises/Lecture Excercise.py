7d5435885db08f771471c0421fecd8fae7644fb3
SnackerSnake <67903971+SnackerSnake@users.noreply.github.com>
2022-12-16 19:47:27 -0500

    Add files via upload

---
+++ LectureExercises/Lecture Excercise.py
@@ -0,0 +1,37 @@
+number_of_weeks = 26
+number_of_days = number_of_weeks * 7
+print(number_of_days)
+
+city_name = "Los Angeles"
+state_name = "California"
+zip_code = "90001-5151"
+address= city_name + ", " + state_name + " " + zip_code
+print(address)
+
+balance = 1000
+withdrawal_amount = 20
+balance-=withdrawal_amount
+print(balance)
+
+#My way
+a = input("Enter a number for a:")
+b = input("Enter a number for b:")
+c = int(a)
+d = int(b)
+#c = float(a) for decimal numbers
+#d = float(b)
+#float then int for deciml numbers
+print("The sum of a and b is ", c+d)
+
+#or teacher way
+
+a = int(input("Enter a number for a:"))
+b = int(input("Enter a number for b:"))
+print("The sum of a and b is ", c+d)
+
+name = input("Enter you name:")
+print("Your name is: " + name)
+print("Hi, " + name)
+#can also use comma w/o space after comma in print
+
+
---
+++ LectureExercises/Lecture Exercise 3.py
@@ -0,0 +1,14 @@
+if High_Cholesterol:
+    print("You have dangerous cholesterol levels!!")
+else:
+    print("Your cholesterol levels are okay.")
+#currentTemp<60
+#heaterIsOn
+
+#Scoring
+#15-20% Tests
+#multiple choice, true/false
+#10%-15% Projects
+#75%-65& Assignments
+#good variables (not x/f)
+#comments, space
